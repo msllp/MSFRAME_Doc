49e1107b4305a315aa2dea83f6fad28f69d43a6c
msllp <48418814+msllp@users.noreply.github.com>
2020-05-20 08:17:15 +0530

    update

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -62,14 +62,14 @@
                                 <ul>
                                     <li><a href="/docs/{{DEFAULT_VERSION}}/releases">Release Notes</a></li>
                                     <li><a href="/docs/{{DEFAULT_VERSION}}/installation">Getting Started</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/routing">Routing</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/blade">Blade Templates</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/authentication">Authentication</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/authorization">Authorization</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/artisan">Artisan Console</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/database">Database</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/eloquent">Eloquent ORM</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/testing">Testing</a></li>
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/routing">Routing</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/blade">Blade Templates</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/authentication">Authentication</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/authorization">Authorization</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/artisan">Artisan Console</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/database">Database</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/eloquent">Eloquent ORM</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/testing">Testing</a></li>--}}
                                 </ul>
                             </div>
                         </div>
@@ -77,14 +77,15 @@
                             <span class="footer_nav_trigger">Resources</span>
                             <div class="footer_nav_contain">
                                 <ul>
-                                    <li><a href="https://laracasts.com">Laracasts</a></li>
-                                    <li><a href="https://laravel-news.com">Laravel News</a></li>
-                                    <li><a href="https://laracon.us">Laracon</a></li>
-                                    <li><a href="https://laracon.eu/">Laracon EU</a></li>
-                                    <li><a href="https://laracon.com.au/">Laracon AU</a></li>
-                                    <li><a href="https://larajobs.com">Jobs</a></li>
-                                    <li><a href="https://certification.laravel.com/">Certification</a></li>
-                                    <li><a href="https://laracasts.com/discuss">Forums</a></li>
+                                    <li>Coming Soon...</li>
+{{--                                    <li><a href="https://laracasts.com">Laracasts</a></li>--}}
+{{--                                    <li><a href="https://laravel-news.com">Laravel News</a></li>--}}
+{{--                                    <li><a href="https://laracon.us">Laracon</a></li>--}}
+{{--                                    <li><a href="https://laracon.eu/">Laracon EU</a></li>--}}
+{{--                                    <li><a href="https://laracon.com.au/">Laracon AU</a></li>--}}
+{{--                                    <li><a href="https://larajobs.com">Jobs</a></li>--}}
+{{--                                    <li><a href="https://certification.laravel.com/">Certification</a></li>--}}
+{{--                                    <li><a href="https://laracasts.com/discuss">Forums</a></li>--}}
                                     <!-- <li><a href="http://www.laravelpodcast.com/">Podcast</a></li> -->
                                     <!-- <li><a href="https://course.testdrivenlaravel.com/">Test-driven Laravel</a></li> -->
                                     <!-- <li><a href="https://statamic.com/">Statamic</a></li> -->
@@ -97,59 +98,45 @@
                             <span class="footer_nav_trigger">Partners</span>
                             <div class="footer_nav_contain">
                                 <ul>
-                                    <li><a href="https://vehikl.com">Vehikl</a></li>
-                                    <li><a href="https://tighten.co">Tighten Co.</a></li>
-                                    <li><a href="https://kirschbaumdevelopment.com/">Kirschbaum</a></li>
-                                    <li><a href="https://www.byte5.net/">Byte 5</a></li>
-                                    <li><a href="https://64robots.com/">64 Robots</a></li>
-                                    <li><a href="https://cubettech.com/">Cubet</a></li>
-                                    <li><a href="https://devsquad.com/">DevSquad</a></li>
-                                    <li><a href="https://www.ideil.com/">Ideil</a></li>
-                                    <li><a href="https://www.cyber-duck.co.uk/how-we-work/technology/laravel?utm_source=Laravel%20Partner&utm_medium=Sponsorship">Cyber-Duck</a></li>
-                                    <li><a href="https://corporate.aboutyou.de/app/uploads/2019/08/INTRO-Pitch-I-AY-Tech.pdf?utm_source=laravelpartnersfindoutmore&utm_medium=socialgroups&utm_campaign=tech">ABOUT YOU</a></li>
-                                    <li><a href="https://www.a2design.biz/">A2 Design</a></li>
-                                    <li><a href="https://romegadigital.com/">Romega Software</a></li>
-                                    <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSeOTE1G6zxSPbKdmQ59UKkL_Rja_ddAyG6Y6xxGdSGAWlNTFA/viewform">Become A Partner</a></li>
-                                </ul>
+                                    <li><a href="https://www.millionsllp.com">Hive Automation</a></li>
+                                     </ul>
                             </div>
                         </div>
                         <div class="nav_col">
                             <span class="footer_nav_trigger">Ecosystem</span>
                             <div class="footer_nav_contain">
                                 <ul>
-                                    <li><a href="https://vapor.laravel.com">Vapor</a></li>
-                                    <li><a href="https://forge.laravel.com">Forge</a></li>
-                                    <li><a href="https://envoyer.io">Envoyer</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/horizon">Horizon</a></li>
-                                    <li><a href="https://lumen.laravel.com">Lumen</a></li>
-                                    <li><a href="https://nova.laravel.com">Nova</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/broadcasting">Echo</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/valet">Valet</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/mix">Mix</a></li>
-                                    <li><a href="https://spark.laravel.com">Spark</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/billing">Cashier</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/homestead">Homestead</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/dusk">Dusk</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/passport">Passport</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/scout">Scout</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/socialite">Socialite</a></li>
-                                    <li><a href="/docs/{{DEFAULT_VERSION}}/telescope">Telescope</a></li>
+
+                                    <li><a>O<sub>3</sub> ERP</a></li>
+                                    <li><a>MS-ERP</a></li>
+                                    <li><a>MS-CRM</a></li>
+                                    <li><a>MS-Flex</a></li>
+                                    <li><a>MS-CCA</a></li>
+{{--                                    <li><a href="https://vapor.laravel.com">Vapor</a></li>--}}
+{{--                                    <li><a href="https://forge.laravel.com">Forge</a></li>--}}
+{{--                                    <li><a href="https://envoyer.io">Envoyer</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/horizon">Horizon</a></li>--}}
+{{--                                    <li><a href="https://lumen.laravel.com">Lumen</a></li>--}}
+{{--                                    <li><a href="https://nova.laravel.com">Nova</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/broadcasting">Echo</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/valet">Valet</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/mix">Mix</a></li>--}}
+{{--                                    <li><a href="https://spark.laravel.com">Spark</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/billing">Cashier</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/homestead">Homestead</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/dusk">Dusk</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/passport">Passport</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/scout">Scout</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/socialite">Socialite</a></li>--}}
+{{--                                    <li><a href="/docs/{{DEFAULT_VERSION}}/telescope">Telescope</a></li>--}}
                                 </ul>
                             </div>
                         </div>
                     </div>
                     <div class="footer_info">
-                        <p class="small">Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel attempts to take the pain out of development by easing common tasks used in most web projects.</p>
-                        <p class="small copyright">Laravel is a Trademark of Taylor Otwell.<br>Copyright &copy; 2011-{{now()->format('Y')}} Laravel LLC.
+                        <p class="small">MS-Framework is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. MS-Framework attempts to take the pain out of development by easing common tasks used in most web projects.</p>
+                        <p class="small copyright">MS-Framework is a Trademark of Mitul Patel.<br>Copyright &copy; 2017-{{now()->format('Y')}} Million Solutions LLP.
                         </p>
-                        <ul class="social_links">
-                            <li><a href="https://twitter.com/laravelphp"><img src="/img/social/twitter.min.svg" alt="Twitter"></a>
-                            </li>
-                            <li><a href="https://github.com/laravel"><img src="/img/social/github.min.svg" alt="GitHub"></a>
-                            </li>
-                            <li><a href="https://discord.gg/mPZNm7A"><img src="/img/social/discord.min.svg" alt="Discord"></a>
-                            </li>
-                        </ul>
                     </div>
                 </div>
             </div>
